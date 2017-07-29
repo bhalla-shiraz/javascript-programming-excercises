37f1c83bc47dff962f4ad6da4301c2c75eb68eb4
Shiraz Bhalla <shiraz.bhalla@copart.com>
2017-07-28 22:00:53 -0500

    Brace Validator

---
+++ Beginner/BracesValidator.js
@@ -0,0 +1,24 @@
+var isValid = function(s) {
+    const map = {
+      '(': ')',
+      '{': '}',
+      '[': ']',
+    }
+    const stack = []
+    let valid = true
+    for(let i = 0; i < s.length; i++) {
+      const item = s[i]
+      if(map.hasOwnProperty(item)) {
+         stack.push(item)
+      } else {
+         let top = stack.pop()
+         if(map[top] !== item) {
+            valid = false
+         }
+      }
+    }
+    return valid
+};
+
+console.log(isValid(['{', '}']))
+console.log(isValid(['{', '[', '}', ']']))
