6d8130499a2369c533b9f3c2484be65d61e4967a
Shiraz Bhalla <shiraz.bhalla@copart.com>
2018-01-27 11:42:33 -0600

    pallindrome improved by converting input to string

--- Basic_programs/pallindrome.js
+++ Basic_programs/pallindrome.js
@@ -1,6 +1,14 @@
 const isPallindrome = (str) => {
-   return str === str.split('').reverse().join('')
+    try {
+        str = str.toString()
+    }
+    catch(err) {
+        return false
+    }
+    return str === str.split('').reverse().join('')
 }
 
 console.log(isPallindrome('javascripttpircsavaj'));
 console.log(isPallindrome('javasj'));
+console.log(isPallindrome(12344321));
+console.log(isPallindrome(true))
